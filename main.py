ad6be35a315617daa927e3a7275b4ffddb4e6db4
ClaudioDev <aleledesma687@gmail.com>
2025-07-22 15:23:49 -0300

    Add filtered data

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import plotly.express as px
+from backend import get_data
 
 st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place: ")
@@ -9,8 +10,10 @@ option = st.selectbox("Select data to view",
                       ("Temperature", "Sky"))
 st.subheader(f"{option} for yhe next {days} days in {place}")
 
-dates = ["2022-25-10", "2022-26-10", "2022-27-10"]
-temperatures = [10, 11, 15]
 
-figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"})
+data = get_data(place, days, option)
+
+
+
+figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (C)"})
 st.plotly_chart(figure)
